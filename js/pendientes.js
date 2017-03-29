3fd55e59689cfbe0bcb0330ec143ab4664452a0d
Edith Mandujano <edithmandujano001@gmail.com>
2017-03-29 11:34:50 -0600

    Proyecto final ya crea las tareas y los pendientes

--- js/pendientes.js
+++ js/pendientes.js
@@ -1,1 +1,12 @@
-alert("todo bien");
+var tareas=document.getElementById('tareas');
+var nombreDeLaTarea=document.getElementById("nombreTarea");
+function addElement(){
+  var tareas=document.getElementById('tareas');
+  var h1 = document.createElement("h1");
+  var ul = document.createElement("ul");
+  h1.innerText = nombreDeLaTarea.value;
+  tareas.appendChild(h1);
+  tareas.appendChild(ul);
+  //var pendientes=prompt("Escribe aqui tus pendientes");
+
+}
